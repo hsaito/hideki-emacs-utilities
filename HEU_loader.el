9ce5886be1171f18e80fa4747eb42dfe77a3987d
Hideki Saito <hidekis@gmail.com>
2012-06-28 02:33:54 -0700

    Added compiler scripts

---
+++ HEU_loader.el
@@ -0,0 +1,13 @@
+;; -*- coding: utf-8 -*-
+;;
+;; Loads up Hideki's Emacs Utilities
+;;
+;; (load-library "HEU_loader.el")
+;;
+
+(require 'anti-space-invader)
+(require 'create-random-buffer)
+(require 'erc-proxy)
+(require 'org-mode-aux)
+(require 'sanitize)
+(require 'utf-32)
